5a378935966391b37b47a83206e36b65de8e70e7
sosokker <sirin_29@hotmail.co.th>
2024-05-11 17:38:57 +0700

    Add type interface of weather data

---
+++ frontend/src/types/weather.ts
@@ -0,0 +1,27 @@
+interface WeatherData {
+  timestamp: string;
+  outdoor_temp: number;
+  outdoor_feels_like: number;
+  outdoor_pressure: number;
+  outdoor_humidity: number;
+  outdoor_weather: string;
+  outdoor_description: string;
+  outdoor_pm25: number;
+  outdoor_pm10: number;
+  indoor_temp: number;
+  indoor_light: number;
+}
+
+interface IndoorWeatherData {
+  avg_indoor_temp: number;
+  avg_indoor_light: number;
+}
+
+interface WeatherData {
+  avg_outdoor_temp: number;
+  avg_outdoor_feels_like: number;
+  avg_outdoor_pressure: number;
+  avg_outdoor_humidity: number;
+  avg_outdoor_pm25: number;
+  avg_outdoor_pm10: number;
+}
